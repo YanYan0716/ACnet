bc9f1c05e5adc2cc47065dae4f19ebd3fbd68216
yanqian <yanqian0716@gmail.com>
2021-07-22 14:51:52 +0800

    .

--- Loss.py
+++ Loss.py
@@ -12,10 +12,18 @@ class myLoss(keras.losses.Loss):
         self.betha = betha
 
     def __call__(self, y_true, y_pred, sample_weight=None):
-        a = self.loss(y_true, y_pred[0]) * self.alpha
-        b = self.loss(y_true, y_pred[1]) * self.alpha
-        c = self.loss(y_true, y_pred[2]) * self.alpha
-        d = self.loss(y_true, y_pred[3]) * self.alpha
-        all_ = self.loss(y_true, y_pred[4]) * self.betha
+        a = self.loss_func(y_true, y_pred[0]) * self.alpha
+        b = self.loss_func(y_true, y_pred[1]) * self.alpha
+        c = self.loss_func(y_true, y_pred[2]) * self.alpha
+        d = self.loss_func(y_true, y_pred[3]) * self.alpha
+        all_ = self.loss_func(y_true, y_pred[4]) * self.betha
+        return (a + b + c + d + all_)
+
+    def loss_func(self, y_true, y_pred):
+        y_pred = tf.nn.log_softmax(y_pred, axis=-1)
+        loss = tf.einsum('mn, mn -> mn', y_true, y_pred)
+        loss = -tf.reduce_mean(loss)
+        return loss
+
         
-        return (a+b+c+d+all_)
+
