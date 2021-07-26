273a12fb2a0c2830ef4054c49862366aefbe7449
yanqian <yanqian0716@gmail.com>
2021-07-26 09:17:55 +0800

    .

--- Loss.py
+++ Loss.py
@@ -3,6 +3,9 @@ import tensorflow.keras as keras
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 
+import config
+
+
 """与paper不同，仅使用CE loss"""
 class myLoss(keras.losses.Loss):
     def __init__(self, alpha=0.25, betha=1.):
@@ -20,6 +23,7 @@ class myLoss(keras.losses.Loss):
         return (a + b + c + d + all_)
 
     def loss_func(self, y_true, y_pred):
+        y_true = tf.one_hot(y_true, config.CLASSES_NUM)
         y_pred = tf.nn.log_softmax(y_pred, axis=-1)
         loss = tf.einsum('mn, mn -> mn', y_true, y_pred)
         loss = -tf.reduce_mean(loss)
