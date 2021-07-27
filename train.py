74aad456b0c1b687a8e0613a80f59a48e2a0390c
yanqian <yanqian0716@gmail.com>
2021-07-27 16:49:23 +0800

    .

--- train.py
+++ train.py
@@ -33,14 +33,14 @@ loss = myLoss(alpha=1., betha=1.)
 # 分段衰减学习率
 lr_schedule = keras.optimizers.schedules.PiecewiseConstantDecay(
     boundaries=[20, 50, 70],
-    values=[0.1, 0.05, 0.001, 0.0005]# [0.5, 0.1, 0.01, 0.005]
+    values=[1., 0.05, 0.001, 0.0005]# [0.5, 0.1, 0.01, 0.005]
 )
 acc_metric = keras.metrics.SparseCategoricalAccuracy(name='accuracy')
 training = CustomFit(model, acc_metric)
 training.compile(
     optimizer=tfa.optimizers.SGDW(
         learning_rate=lr_schedule,
-        momentum=0.9,
+        momentum=0.95,
         weight_decay=5e-6
     ),
     loss=loss,
