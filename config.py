340febc651312cd494ebe36ab4503ae0575dec1b
yanqian <yanqian0716@gmail.com>
2021-07-29 14:19:31 +0800

    .

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DATA_PATH = '../input/cub-200-2011/train.csv'
 DATA_TEST = '../input/cub-200-2011/test.csv'
 SIZE = 512
 IMG_SIZE = 448
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 CLASSES_NUM = 200
 
 FTS_SIZE = 28
