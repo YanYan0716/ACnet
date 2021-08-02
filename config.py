e65822d14dd5289db583e3914f8e5e6bf7fea7ee
yanqian <yanqian0716@gmail.com>
2021-08-02 10:14:44 +0800

    .

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DATA_PATH = '../input/cifar10/cifar/train.csv'
 DATA_TEST = '../input/cifar10/cifar/test.csv'
 SIZE = 130
 IMG_SIZE = 112
-BATCH_SIZE = 16
+BATCH_SIZE = 24
 CLASSES_NUM = 10
 
 FTS_SIZE = 28
@@ -13,7 +13,7 @@ FIRST_SEAGE = True
 LR_STEP = 100
 INIT_LR = 0.02
 LOG_BATCH = 100
-EVAL_EPOCH = 2
+EVAL_EPOCH = 1
 LOAD_PATH = './model.h5'
 SAVE_PATH = './model.h5'
 L2 = tf.keras.regularizers.l2(5e-6)
