73b718bb9a2f9c30b34272f8e869e87ec423a48a
yanqian <yanqian0716@gmail.com>
2021-08-02 15:21:07 +0800

    .

--- train_.py
+++ train_.py
@@ -58,8 +58,7 @@ for epoch in range(config.MAX_EPOCH):
                 config.FIRST_SEAGE) + '    [max_epoch: %3d]' % config.MAX_EPOCH + '[epoch:%3d/' % (epoch + 1) \
                   + 'idx: %4d]' % flag + '[Loss:%.4f' % (loss.numpy()) + ', ACC: %.2f]' % (
                           acc_metric.result().numpy() * 100))
-            if flag >= 500:
-                break
+
     acc_metric.reset_states()
     test_flag = 0
     if (epoch + 1) % config.EVAL_EPOCH == 0:
@@ -67,8 +66,7 @@ for epoch in range(config.MAX_EPOCH):
             test_flag += 1
             y_pred = model.predict(img)
             acc_metric.update_state(label, y_pred[-1])
-            if test_flag >= 5000:
-                break
+
         print(
             f'[testing ...]' + '[epoch:%3d/' % (epoch + 1) + ',ACC: %.2f]' % (acc_metric.result().numpy() * 100)
         )
