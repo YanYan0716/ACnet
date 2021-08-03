7e5fa14c748ad07593744e0be25666be19a8ec85
yanqian <yanqian0716@gmail.com>
2021-08-03 11:23:33 +0800

    .

--- Attention.py
+++ Attention.py
@@ -78,10 +78,10 @@ class Attention(keras.layers.Layer):
 
     def call(self, inputs):
         # img_fts1 = self.ASPP(inputs)
-        # img_fts1 = self.relu(self.BN_1(self.conv_1(inputs)))
-        # img_fts1 = self.BN_2(self.conv_2(img_fts1))
-        img_fts1 = self.relu(self.conv_1(inputs))
-        img_fts1 = self.conv_2(img_fts1)
+        img_fts1 = self.relu(self.BN_1(self.conv_1(inputs)))
+        img_fts1 = self.BN_2(self.conv_2(img_fts1))
+        # img_fts1 = self.relu(self.conv_1(inputs))
+        # img_fts1 = self.conv_2(img_fts1)
 
         img_fts2 = self.GAP(img_fts1)
         img_fts2 = tf.expand_dims(img_fts2, axis=1)
